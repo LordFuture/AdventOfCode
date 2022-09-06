af7d8fd0344b941691011474645bda56f65dbb84
Steven Rodney <srodney@kingsleynapley.co.uk>
2022-09-06 15:16:58 +0100

    Completed Day 4

---
+++ AOC_2015/Day_04/ex_01.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Code used to solve Advent of Code 2015 Day 4 exercise 1
+Advent of Code 2015, Day 04, Exercise 1
+# https://adventofcode.com/2015
+"""
+
+import os
+import hashlib
+
+
+def read_secret_key(file_name):
+    """
+    It opens the file, reads the first line, and returns it
+
+    :param file_name: The name of the file that contains the secret key
+    :return: The line read from the file.
+    """
+    with open(file_name, "r", encoding="utf-8") as file:
+        while True:
+            if line := file.readline():
+                return line
+            break
+
+
+
+def get_hash(file_name):
+    """
+    It takes a file name as input, reads the secret key from the file, and then
+    iterates through a million numbers, calculating the MD5 hash of the secret key
+    concatenated with the number. If the first five characters of the hash are
+    zeroes, it returns the number. If it doesn't find a number that works, it
+    returns zero
+
+    :param file_name: The name of the file that contains the secret key
+    :return: The number of iterations it took to find the hash.
+    """
+    secret_key = read_secret_key(file_name)
+    for i in range(1000000):
+        calculated_hash = hashlib.md5((secret_key + str(i)).encode()).hexdigest()
+        if calculated_hash[:5] == '00000':
+            return i
+
+    return 0
+
+
+
+def main():
+    """
+    It takes a file name as input, reads the file, and returns the lowest number
+    that can be appended to the file's contents to produce a hash that starts with
+    five zeros
+    """
+    os.system("cls || clear")
+
+    file_name_test_file = "C:/source/repos/AdventOfCode/AOC_2015/Day_04/test_file.txt"
+    file_name_puzzle_file = "C:/source/repos/AdventOfCode/AOC_2015/Day_04/puzzle_input_file.txt"
+    test_file_answer = get_hash(file_name_test_file)
+    puzzle_file_answer = get_hash(file_name_puzzle_file)
+    print(f"test file: {test_file_answer}")
+    print(f"puzzle file: {puzzle_file_answer}")
+
+if __name__ == '__main__':
+    main()
